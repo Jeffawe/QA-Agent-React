a86f7d8b4e90e68a8aad9ed19f9ca55b05c8e2a1
Jeffery Ozoekwe-Awagu <72825145+Jeffawe@users.noreply.github.com>
2025-10-28 18:12:09 -0400

    Fixing done sequence

--- src/pages/UpdatesPage.tsx
+++ src/pages/UpdatesPage.tsx
@@ -17,7 +17,7 @@ const BASE_URL = import.meta.env.VITE_API_URL;
 // Main Updates Page Component with Tabs
 const UpdatesPage: React.FC = () => {
   const [connected, setConnected] = useState(false);
-  const [done, setDone] = useState(false);
+  const [done, setDone] = useState(true);
   const [updates, setUpdates] = useState<PageDetails[]>([]);
   const [logs, setLogs] = useState<string[]>([]);
   const [donePageModalOpen, setDonePageModalOpen] = useState(false);
